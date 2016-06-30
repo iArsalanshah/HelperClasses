1586f29eeaff7c3ca5b4d76d84c0fa57ec26b683
Syed Arsalan Shah <iArsalanshah@users.noreply.github.com>
2016-06-30 11:00:02 +0500

    Create InternetConnectionHelper.java
    checks internet connection

---
+++ InternetConnectionHelper.java
@@ -0,0 +1,13 @@
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class InternetConnectionHelper {
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting() && activeNetwork.isAvailable();
+    }
+}
